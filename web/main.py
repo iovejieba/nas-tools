ba55533a077f9cd224040d2fcf244b030931a5a9
iceveil <im@iceveil.com>
2023-03-01 05:17:39 +0000

    更改Flask超时时间为300s

--- web/main.py
+++ web/main.py
@@ -50,6 +50,7 @@ ConfigLock = Lock()
 # Flask App
 App = Flask(__name__)
 App.config['JSON_AS_ASCII'] = False
+App.config['timeout'] = 300
 App.secret_key = os.urandom(24)
 App.permanent_session_lifetime = datetime.timedelta(days=30)
 
